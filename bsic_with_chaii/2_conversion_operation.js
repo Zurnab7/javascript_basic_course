1d649f8791d923dcf36fa3362756c11ee5dc63d9
Zurnab7 <155717759+Zurnab7@users.noreply.github.com>
2024-09-03 11:48:44 +0000

    2 september

---
+++ bsic_with_chaii/2_conversion_operation.js
@@ -0,0 +1,43 @@
+let score = 33;
+let age =56;
+console.log(typeof score);
+console.log(typeof (age));
+// '33'=> 33
+//"33abce"=> Nan(not a number)
+//true =>1, false =>0,
+let isLogegedIn = 1;
+let booleanLoggedIn = Boolean(isLogegedIn);
+console.log(booleanLoggedIn
+)
+// 1=> true, 0=> false, ""=>false, "Hitexh"=>true
+let isNumber = 33;
+let stringisNumber = String(isNumber);
+console.log(stringisNumber)
+console.log(typeof stringisNumber)
+// **************OPERATIONS*****************
+//console.log(2+2)
+//console.log(2-2)
+//console.log(2/2)
+//console.log(2*2)
+//console.log(2**2)2 ki power 2  
+//console.log(2**7) 2 ki power 7
+//console.log(2%5) modulus=remiander value
+//console.log("1"+2) =>12 string strat me to agy sbko string me convert kia jeye ga
+//console.log(1+"2") => 12
+//console.log("1"+2 + 2) => 122
+//console.log(1+2 +"2") => 32string end me to phly conversion ho jaye gi
+//use pranthese to give direction
+console.log((3+9) * 6);
+//***************Comparison*********************
+// console.log( null > 0)
+// console.log( null < 0)
+// console.log( null = 0)
+// console.log( null == 0)
+// console.log( null >= 0)
+//console.log( undefined < 0)
+//console.log( undefined > 0)
+//console.log( undefined == 0)
+//avoid above mentioned like comparison in js that;s not professional 
+// === strickly check and also data type 
+//console.log("2"==2); will show both equal as convert string into data type
+//console.log("2"==2); strickly checked, will not show equal as strings != number
